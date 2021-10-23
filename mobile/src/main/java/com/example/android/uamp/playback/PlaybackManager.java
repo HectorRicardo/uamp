f68c722523f8b2ca671d8e9381b09d7b6eb38c94
Hector Ricardo <hectorricardomendez@gmail.com>
2021-10-22 20:23:54 -0500

    Enabling seekbar

--- mobile/src/main/java/com/example/android/uamp/playback/PlaybackManager.java
+++ mobile/src/main/java/com/example/android/uamp/playback/PlaybackManager.java
@@ -176,6 +176,7 @@ public class PlaybackManager implements Playback.Callback {
                 PlaybackStateCompat.ACTION_PLAY_FROM_MEDIA_ID |
                 PlaybackStateCompat.ACTION_PLAY_FROM_SEARCH |
                 PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
+                PlaybackStateCompat.ACTION_SEEK_TO |
                 PlaybackStateCompat.ACTION_SKIP_TO_NEXT;
         if (mPlayback.isPlaying()) {
             actions |= PlaybackStateCompat.ACTION_PAUSE;
